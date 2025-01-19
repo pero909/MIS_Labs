53b46aaaae32e528e6fc57f6222d7354b6894906
pero <perovasev@gmail.com>
2025-01-19 13:24:20 +0100

    lab 4 Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,20 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'screens/joke_type_screen.dart';
-import 'screens/random_joke_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'screens/calendar_screen.dart';
+import 'screens/map_screen.dart';
 
-import 'services/notification_service.dart';
-
-
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  NotificationService().init(); // Initialize the notification service
-  NotificationService().scheduleNotification();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  runApp(MyApp());
-}
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("Handling a background message: ${message.messageId}");
-  String? token = await FirebaseMessaging.instance.getToken();
-  print("Device Token: $token");
+void main() {
+  runApp(ExamScheduleApp());
 }
 
-class MyApp extends StatelessWidget {
+class ExamScheduleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Joke App (191553)',
+      title: 'Распоред на испити',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
-      routes: {
-        '/randomJoke': (context) => RandomJokeScreen(),
-      },
+      home: CalendarScreen(),
     );
   }
 }
